fe6e1dfec769e688fc5126aa844617f856adcbda
Дмитрий <dimas.savin1990@gmail.com>
2022-11-16 22:43:33 -0500

    start task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Vimeo from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+function onTimeUpdate() { };
+
+console.log(player);
+
+player.on('timeupdate', onTimeUpdate);
