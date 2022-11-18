282429081806670d87b1724ec36f4e4cd232eacf
Дмитрий <dimas.savin1990@gmail.com>
2022-11-17 21:20:02 -0500

    add files task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,8 +3,24 @@ import Vimeo from '@vimeo/player';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe);
 
-function onTimeUpdate() { };
-
-console.log(player);
+const onTimeUpdate = (e) => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
+};
 
 player.on('timeupdate', onTimeUpdate);
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });;
