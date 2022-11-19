22d7354bd174117fb4a1dc80e1be212ca3280b24
Дмитрий <dimas.savin1990@gmail.com>
2022-11-19 17:19:37 -0500

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,26 @@
 import throttle from 'lodash.throttle';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    submitBtn: document.querySelector('button'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+}
 
-const form = document.querySelector('.feedback-form');
-const submitBtn = document.querySelector('button');
-const input = document.querySelector('input');
-const textarea = document.querySelector('textarea');
-
-const onFormCheck = (e) => {
-    
-    localStorage.setItem('feedback-form-state', JSON.stringify(e.target.value));
+const onFormCheck = () => {
+    localStorage.setItem('feedback-form-state',
+    JSON.stringify({ email: refs.input.value, message: refs.textarea.value }));
 };
+
 const onSubmitClick = (e) => {
     e.preventDefault();
     const formObj = {
-        email: input.value,
-        message: textarea.value,
+        email: refs.input.value,
+        message: refs.textarea.value,
     };
     localStorage.clear();
-    form.reset();
+    refs.form.reset();
     console.log(formObj);
 };
 
-form.addEventListener('input', throttle(onFormCheck, 500));
-submitBtn.addEventListener('click', onSubmitClick);
+refs.form.addEventListener('input', throttle(onFormCheck, 500));
+refs.submitBtn.addEventListener('click', onSubmitClick);
