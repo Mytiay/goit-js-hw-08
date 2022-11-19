8a3179c28de6233a07a290c2b89cf7f10bf7d729
Дмитрий <dimas.savin1990@gmail.com>
2022-11-18 21:39:12 -0500

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,15 +2,22 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const submitBtn = document.querySelector('button');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
 
 const onFormCheck = (e) => {
-    e.preventDefault();
+    
     localStorage.setItem('feedback-form-state', JSON.stringify(e.target.value));
 };
 const onSubmitClick = (e) => {
+    e.preventDefault();
+    const formObj = {
+        email: input.value,
+        message: textarea.value,
+    };
     localStorage.clear();
     form.reset();
-    console.log(email)
+    console.log(formObj);
 };
 
 form.addEventListener('input', throttle(onFormCheck, 500));
