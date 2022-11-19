71c04041bd5404d9b808c3089d43e60093d49d25
Дмитрий <dimas.savin1990@gmail.com>
2022-11-18 20:03:17 -0500

    task2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe);
@@ -7,20 +8,20 @@ const onTimeUpdate = (e) => {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
 };
 
-player.on('timeupdate', onTimeUpdate);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
 player
   .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
   .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
+    if (!localStorage.getItem('videoplayer-current-time'));
+    return;
   })
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
+        if (localStorage.getItem('videoplayer-current-time') === 0);
         break;
-
+      
       default:
-        // some other error occurred
         break;
     }
   });;
